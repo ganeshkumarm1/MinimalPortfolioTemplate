9f584ede210c09d59d95e09faa4086b44eb96351
GaneshmKumar <ganeshkumar.m1996@gmail.com>
2017-06-02 22:07:05 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var divs = ['#home', '#skills', '#projects', '#contact'];
+var index = 0;
+
+$(document).ready(function() {
+	$('#next').on('click', function() {
+		if(index == 3)
+		{
+			$("#next").attr("disabled", true);
+		}
+		else
+		{
+			$("#next").attr("disabled", false);
+			$(divs[index]).fadeOut('slow', function() {
+				index ++;
+				console.log(index);
+				$(divs[index]).fadeIn('slow');
+			});
+		}
+	});
+
+	$('#prev').on('click', function() {
+		if(index == 0)
+		{
+			$("#prev").attr("disabled", true);
+		}
+		else
+		{
+			$("#prev").attr("disabled", false);
+			$(divs[index]).fadeOut('slow', function() {
+				index --;
+				console.log(index);
+				$(divs[index]).fadeIn('slow');
+			});
+		}
+	});
+});
